57d564699f6c9fb91cf34671116cd4a2b47597e7
Dhruv Khator <dhruvkhator19@gmail.com>
2022-04-02 10:39:52 +0530

    Added

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const router = require('./Router/index');
+
+const app = express();
+
+const port = 4567;
+const hostname = 'localhost';
+app.use(bodyParser.json());
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+  app.use(cors(corsOpts));
+
+
+const serverDB = 'mongodb+srv://dhruv:cojW6paNRPkrH73g@cluster0.byqsk.mongodb.net/zomato_clone?retryWrites=true&w=majority';
+
+app.use(express.json());
+app.use('/', router);
+
+mongoose.connect(serverDB,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(res => {
+        app.listen(port, hostname, () => {
+            console.log(`Server is running at http://${hostname}:${port}`);
+        })
+    })
+    .catch(err => console.log(err));
